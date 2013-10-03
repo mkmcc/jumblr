4071c18460853eeeb582ee5e14a469638fc4c9de
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-10-02 23:49:21 -0700

    cleanup

--- remake-game-list.el
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-":"; exec emacs --no-site-file --script "$0" -- "$@" # -*-emacs-lisp-*-
-(package-initialize)
-(load-file "jumblr.el")
-(setq jumblr-precompute-file "~/jumblr-games.el")
-(jlr-recompute-games 500 5 9)
